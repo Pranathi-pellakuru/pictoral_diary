34d9ad022642323a6bc67a7a2ed602fa2ad13221
Pranathi-Cumulations <pranathi.p@cumulations.com>
2023-08-30 14:54:20 +0530

    Add. Firebase Analytics

--- src/pages/UnSplashPage.js
+++ src/pages/UnSplashPage.js
@@ -6,6 +6,7 @@ import {
   Image,
   TouchableOpacity,
   Button,
+  Keyboard,
 } from 'react-native';
 import SaerchBar from '../components/SearchBar';
 import {useDispatch} from 'react-redux';
@@ -16,21 +17,37 @@ export default function UnSplashPage() {
   const [isloading, setIsloading] = useState(true);
   const [selectedItem, setSelectedItem] = useState(null);
   const [selectedURL, setSelectedURL] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [submittedText, setSubmittedText] = useState('');
 
   const dispatch = useDispatch();
 
   const load = text => {
+    setSubmittedText(text);
     var url = `https://api.unsplash.com//search/photos/?client_id=EZohUNOO5yQZ0ErhjVgLqx6OPfShBP7e6nKlctUGUEo&query=${text}&page=1`;
     fetch(url)
       .then(res => res.json())
       .then(data => {
         setData(data.results);
         setIsloading(false);
+        setPageNumber(pageNumber + 1);
+      });
+  };
+
+  const loadMore = () => {
+    var url = `https://api.unsplash.com//search/photos/?client_id=EZohUNOO5yQZ0ErhjVgLqx6OPfShBP7e6nKlctUGUEo&query=${submittedText}&page=${pageNumber}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data1 => {
+        setData([...data, ...data1.results]);
+        setPageNumber(pageNumber + 1);
       });
   };
 
   const onSubmit = text => {
+    console.log(text);
     load(text);
+    Keyboard.dismiss();
   };
 
   const onImageTouched = item => {
@@ -71,6 +88,8 @@ export default function UnSplashPage() {
           keyExtractor={item => item.id}
           style={styles.flatlist}
           numColumns={2}
+          onEndReached={loadMore}
+          onEndReachedThreshold={0.1}
         />
       )}
       <View style={styles.floatingbtn}>
