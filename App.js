0d67954209da6823279e569eb553d1dab9870b1a
Pranathi-Cumulations <pranathi.p@cumulations.com>
2023-08-30 18:34:43 +0530

    add . Phone sign In by firebase

--- App.js
+++ App.js
@@ -14,13 +14,21 @@ import Home from './src/pages/HomePage';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store';
+import PhoneSignIn from './src/pages/AuthPage';
 
 const Stack = createNativeStackNavigator();
 function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator initialRouteName="LogIn">
+          <Stack.Screen
+            name="LogIn"
+            component={PhoneSignIn}
+            options={{
+              title: 'LogIn',
+            }}
+          />
           <Stack.Screen
             name="Home"
             component={Home}
